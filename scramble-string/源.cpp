e89b24b24022be923584d43dca8aa2de5ca137a9
zhangkai <871227769@qq.com>
2019-04-27 16:38:16 +0800

    Update 源.cpp

--- scramble-string/源.cpp
+++ scramble-string/源.cpp
@@ -1,3 +1,54 @@
+/*Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+
+Example 1:
+
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+
+Example 2:
+
+Input: s1 = "abcde", s2 = "caebd"
+Output: false*/
+
+
 #include<cstdio>
 #include<string>
 #include<iostream>
@@ -13,7 +64,7 @@ public:
 			return false;
 
 		int len = s1.size();
-		int c[26] = { 0 };//c[25]±íÊ¾¸ÃÊı×é´óĞ¡Îª25£¡£¡£¡Òª´Óc[26]²Å¶Ô£¡£¡£¡
+		int c[26] = { 0 };//c[25]è¡¨ç¤ºè¯¥æ•°ç»„å¤§å°ä¸º25ï¼ï¼ï¼è¦ä»c[26]æ‰å¯¹ï¼ï¼ï¼
 		for (int i = 0; i < len; ++i)
 		{
 			c[s1[i] - 'a']++;
