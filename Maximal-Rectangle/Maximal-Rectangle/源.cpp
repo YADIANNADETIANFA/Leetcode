e055c0466b8f14b3abfeb9eb27cc719a1751afcd
zhangkai <871227769@qq.com>
2019-06-14 19:59:15 +0800

    Update 源.cpp

--- Maximal-Rectangle/Maximal-Rectangle/源.cpp
+++ Maximal-Rectangle/Maximal-Rectangle/源.cpp
@@ -11,9 +11,14 @@ Input:
 ]
 Output: 6
 
-	 * ����ⷨ����84. Largest Rectangle in Histogram
-	 * ��ÿһ�п�����ֱ��ͼ���������������
+	 * 本体解法基于84. Largest Rectangle in Histogram
+	 * 把每一行看成求直方图中最大矩形面积问题
 */
+
+/*
+当只有t层的时候，已算出其当时的矩形值了，
+现在我们要变为t+1层，要体现出新一层参与的必要性，即，最大矩形是肯定要含有最新一层的“1”的！所以，一旦哪一列出现了凹口，
+该列就不可能成为“平整的”矩形的一员了，要么从中间从头开记，要么清零处理。具体看说明图*/
 #include<cstdio>
 #include<iostream>
 #include<vector>
@@ -31,11 +36,11 @@ public:
 
 		int m = matrix.size();
 		int n = matrix[0].size();
-		vector<int> h(n, 0);//�洢��ĳһ�������ϣ�ÿһ�е�����1�ĸ���
+		vector<int> h(n, 0);//存储从某一行起向上，每一列的连续1的个数
 		int maxS = 0;
 		stack<int> st;
 		
-		for (int i = 0; i < m; ++i)//��ǰ��i����������1�ĸ���������������Ϊ0��
+		for (int i = 0; i < m; ++i)//求当前第i行往上连续1的个数，不连续就置为0，
 		{
 			for (int j = 0; j < n; ++j)
 			{
